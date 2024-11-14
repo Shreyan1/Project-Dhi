456b6232934d1db409a34b91b94e3857fa1bab13
Shreyan1 <shreyan.github@gmail.com>
2024-11-14 18:56:05 +0530

    Whisper-tiny en integrated

--- Speech2Text/stt.py
+++ Speech2Text/stt.py
@@ -1,2 +1,33 @@
 ### Hugging face STT Test for implementation
 
+# pip install transformers torch
+
+import os
+import warnings
+warnings.filterwarnings('ignore')  # Suppress all other warnings
+os.environ['TRANSFORMERS_VERBOSITY'] = 'error'  # Suppress transformer warnings
+
+'''
+
+Reason for using : 
+    os.environ['TRANSFORMERS_VERBOSITY'] = 'error'
+
+Had this stupid warning for which I could not find any fix for the life of me anywhere, and as it was not causing any issues with the workflow, I suprressed it.
+Will look at this issue later or if you can find any fix, please do go ahead and raise a PR. 
+
+This is the warning - 
+####
+    The attention mask is not set and cannot be inferred from input because pad token is same as eos token. 
+    As a consequence, you may observe unexpected behavior. 
+    Please pass your input's `attention_mask` to obtain reliable results.
+####
+'''
+
+from transformers import pipeline
+
+model = pipeline("automatic-speech-recognition", 
+                 model="openai/whisper-tiny.en")
+
+# Transcribe the audio file with a forced language token in the text
+transcription = model("Speech2Text/Dhi_test1.wav")
+print(transcription["text"])
